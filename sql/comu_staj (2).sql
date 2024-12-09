bb8481e25eb534f79d0d65bbeeeb90e680833dd3
mounikavemulapalli <124512238+mounikavemulapalli@users.noreply.github.com>
2024-12-09 15:29:20 +0530

    SQL updated

--- sql/comu_staj (2).sql
+++ sql/comu_staj (2).sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 23, 2022 at 11:38 PM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 8.1.5
+-- Generation Time: Dec 09, 2024 at 10:57 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,25 +24,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `department`
---
-
-CREATE TABLE `department` (
-  `id` int(11) NOT NULL,
-  `bolum_ad` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `department`
---
-
-INSERT INTO `department` (`id`, `bolum_ad`) VALUES
-(7, 'Computer Programming'),
-(9, 'Construction Technician');
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `advisor_details`
 --
 
@@ -51,7 +32,7 @@ CREATE TABLE `advisor_details` (
   `title_id` int(11) NOT NULL,
   `danisman_id` int(11) NOT NULL,
   `bolum_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Dumping data for table `advisor_details`
@@ -65,112 +46,45 @@ INSERT INTO `advisor_details` (`id`, `title_id`, `danisman_id`, `bolum_id`) VALU
 -- --------------------------------------------------------
 
 --
--- Table structure for table `terms`
+-- Table structure for table `department`
 --
 
-CREATE TABLE `terms` (
+CREATE TABLE `department` (
   `id` int(11) NOT NULL,
-  `donem_yil` year(4) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `bolum_ad` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
--- Dumping data for table `terms`
+-- Dumping data for table `department`
 --
 
-INSERT INTO `terms` (`id`, `donem_yil`) VALUES
-(1, 2022);
+INSERT INTO `department` (`id`, `bolum_ad`) VALUES
+(7, 'Computer Programming'),
+(9, 'Construction Technician');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Table structure for table `internship_date`
 --
 
-CREATE TABLE `users` (
+CREATE TABLE `internship_date` (
   `id` int(11) NOT NULL,
-  `ad` varchar(255) NOT NULL,
-  `soyad` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `PasswordHash` varchar(255) NOT NULL,
-  `rol_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `donem_id` int(11) NOT NULL,
+  `haftalik_gun_sayi` int(11) NOT NULL,
+  `staj_baslangic` date NOT NULL,
+  `staj_bitis` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
--- Dumping data for table `users`
+-- Dumping data for table `internship_date`
 --
 
-INSERT INTO `users` (`id`, `ad`, `soyad`, `email`, `PasswordHash`, `rol_id`) VALUES
-(1, 'Ümit', 'Demir', 'müdür@comu.edu.tr', '304e19039eda30537eb597547b43f2d3', 1),
-(43, 'Yelda', 'Fırat', 'yelda@comu.ogr.edu.tr', '4e6fec3630db86b46933bfef7b8f8d48', 2),
-(44, 'Personel', 'Test', 'personel@comu.edu.tr', '24d5cf4a7c1c01020a4131757f1406f7', 3),
-(45, 'Aytuğ', 'Tuncer', '200929029@ogr.comu.edu.tr', '475669a24cedc37ff25aedf47397aa7c', 4),
-(48, 'Murat', 'Dalkırıç', 'murat@comu.ogr.edu.tr', '7b6bb36f3b0e576af4fff416d7d7a2fa', 2),
-(49, 'Berke', 'Altuntaş', '200624029@ogr.comu.edu.tr', '6cc2d13dba8f8b5678bb299f55e69140', 4),
-(52, 'test', 'danisman', 'testdanisman@comu.edu.tr', '5675cbeda1e9fb22910ed7ac90fb1dac', 2);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `student_details`
---
-
-CREATE TABLE `student_details` (
-  `id` int(11) NOT NULL,
-  `ogrenci_id` int(11) NOT NULL,
-  `danisman_id_fk` int(11) NOT NULL,
-  `bolum_id_fk` int(11) NOT NULL,
-  `ogrenci_no` bigint(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `student_details`
---
-
-INSERT INTO `student_details` (`id`, `ogrenci_id`, `danisman_id_fk`, `bolum_id_fk`, `ogrenci_no`) VALUES
-(9, 45, 43, 7, 200929029),
-(10, 49, 48, 9, 200624029);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `roles`
---
-
-CREATE TABLE `roles` (
-  `id` int(11) NOT NULL,
-  `role_ad` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `roles`
---
-
-INSERT INTO `roles` (`id`, `role_ad`) VALUES
-(1, 'manager'),
-(2, 'danışman'),
-(3, 'personel'),
-(4, 'öğrenci');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `social_security`
---
-
-CREATE TABLE `social_security` (
-  `id` int(11) NOT NULL,
-  `ad` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `social_security`
---
-
-INSERT INTO `social_security` (`id`, `ad`) VALUES
-(1, 'BAĞKUR’dan sağlık hizmeti alıyor'),
-(2, 'SGK’dan sağlık hizmeti alıyor\r\n'),
-(3, 'EMEKLİ SANDIĞI’ndan sağlık hizmeti alıyor'),
-(4, 'Herhangi bir sigortam bulunmamakta');
+INSERT INTO `internship_date` (`id`, `donem_id`, `haftalik_gun_sayi`, `staj_baslangic`, `staj_bitis`) VALUES
+(6, 1, 5, '2022-05-18', '2022-05-30'),
+(12, 1, 5, '2022-05-18', '2022-05-30'),
+(14, 1, 6, '2022-04-22', '2022-04-02'),
+(15, 1, 6, '2022-06-01', '2022-08-20');
 
 -- --------------------------------------------------------
 
@@ -198,7 +112,7 @@ CREATE TABLE `internship_registration` (
   `sigorta_giris_onay` tinyint(1) NOT NULL DEFAULT 0,
   `sigorta_cikis_onay` tinyint(1) NOT NULL DEFAULT 0,
   `create_tarih` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Dumping data for table `internship_registration`
@@ -206,31 +120,89 @@ CREATE TABLE `internship_registration` (
 
 INSERT INTO `internship_registration` (`id`, `ogrenci_id`, `tc`, `tel`, `Internship_date_id`, `sigorta`, `adres`, `mudur_onay`, `danisman_onay`, `k_ad`, `k_adres`, `k_hizmet_alan`, `k_no`, `k_faks_no`, `k_eposta`, `k_webadres`, `sigorta_giris_onay`, `sigorta_cikis_onay`, `create_tarih`) VALUES
 (7, 45, 33302383892, 5445678503, 12, 1, 'Çanakkale/Gelibolu', 1, 1, 'Çanakkale Belediyesi', 'İsmet Paşa Mahallesi Demircioğlu Caddesi No:132 17100 Çanakkale', 'Bilgi İşlem', '444 17 17', '0 286 212 39 91', 'canakkale.belediyesi@bel.com', 'https://www.canakkale.bel.tr/', 1, 0, '2022-05-01 15:22:47'),
-(8, 49, 11111111111, 5111111111, 6, 1, 'Çanakkale/Çan', 0, 1, '300dpi', 'Çanakkale/Barbaros ', 'Yazılım', '5445689603', '2125668501', '300dpi@gmail.com', '300dpi.com', 0, 0, '2022-05-01 15:29:50');
+(8, 49, 11111111111, 5111111111, 6, 1, 'Çanakkale/Çan', 1, 1, '300dpi', 'Çanakkale/Barbaros ', 'Yazılım', '5445689603', '2125668501', '300dpi@gmail.com', '300dpi.com', 0, 0, '2022-05-01 15:29:50');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Internship_date`
+-- Table structure for table `roles`
 --
 
-CREATE TABLE `Internship_date` (
+CREATE TABLE `roles` (
   `id` int(11) NOT NULL,
-  `donem_id` int(11) NOT NULL,
-  `haftalik_gun_sayi` int(11) NOT NULL,
-  `staj_baslangic` date NOT NULL,
-  `staj_bitis` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `role_ad` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
--- Dumping data for table `Internship_date`
+-- Dumping data for table `roles`
 --
 
-INSERT INTO `Internship_date` (`id`, `donem_id`, `haftalik_gun_sayi`, `staj_baslangic`, `staj_bitis`) VALUES
-(6, 1, 5, '2022-05-18', '2022-05-30'),
-(12, 1, 5, '2022-05-18', '2022-05-30'),
-(14, 1, 6, '2022-04-22', '2022-04-02'),
-(15, 1, 6, '2022-06-01', '2022-08-20');
+INSERT INTO `roles` (`id`, `role_ad`) VALUES
+(1, 'manager'),
+(2, 'danışman'),
+(3, 'personel'),
+(4, 'öğrenci');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `social_security`
+--
+
+CREATE TABLE `social_security` (
+  `id` int(11) NOT NULL,
+  `ad` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `social_security`
+--
+
+INSERT INTO `social_security` (`id`, `ad`) VALUES
+(1, 'BAĞKUR’dan sağlık hizmeti alıyor'),
+(2, 'SGK’dan sağlık hizmeti alıyor\r\n'),
+(3, 'EMEKLİ SANDIĞI’ndan sağlık hizmeti alıyor'),
+(4, 'Herhangi bir sigortam bulunmamakta');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student_details`
+--
+
+CREATE TABLE `student_details` (
+  `id` int(11) NOT NULL,
+  `ogrenci_id` int(11) NOT NULL,
+  `danisman_id_fk` int(11) NOT NULL,
+  `bolum_id_fk` int(11) NOT NULL,
+  `ogrenci_no` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `student_details`
+--
+
+INSERT INTO `student_details` (`id`, `ogrenci_id`, `danisman_id_fk`, `bolum_id_fk`, `ogrenci_no`) VALUES
+(9, 45, 43, 7, 200929029),
+(10, 49, 48, 9, 200624029);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `terms`
+--
+
+CREATE TABLE `terms` (
+  `id` int(11) NOT NULL,
+  `donem_yil` year(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `terms`
+--
+
+INSERT INTO `terms` (`id`, `donem_yil`) VALUES
+(1, '2022');
 
 -- --------------------------------------------------------
 
@@ -241,7 +213,7 @@ INSERT INTO `Internship_date` (`id`, `donem_id`, `haftalik_gun_sayi`, `staj_basl
 CREATE TABLE `titles` (
   `id` int(11) NOT NULL,
   `unvan_ad` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Dumping data for table `titles`
@@ -252,15 +224,37 @@ INSERT INTO `titles` (`id`, `unvan_ad`) VALUES
 (2, 'Doç. Dr.'),
 (3, 'Dr. Öğr.');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `ad` varchar(255) NOT NULL,
+  `soyad` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `PasswordHash` varchar(255) NOT NULL,
+  `rol_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `ad`, `soyad`, `email`, `PasswordHash`, `rol_id`) VALUES
+(1, 'Ümit', 'Demir', 'müdür@comu.edu.tr', '304e19039eda30537eb597547b43f2d3', 1),
+(43, 'Yelda', 'Fırat', 'yelda@comu.ogr.edu.tr', '4e6fec3630db86b46933bfef7b8f8d48', 2),
+(45, 'Aytuğ', 'Tuncer', '200929029@ogr.comu.edu.tr', '475669a24cedc37ff25aedf47397aa7c', 4),
+(48, 'Murat', 'Dalkırıç', 'murat@comu.ogr.edu.tr', '7b6bb36f3b0e576af4fff416d7d7a2fa', 2),
+(49, 'Berke', 'Altuntaş', '200624029@ogr.comu.edu.tr', '6cc2d13dba8f8b5678bb299f55e69140', 4),
+(52, 'test', 'danisman', 'testdanisman@comu.edu.tr', '5675cbeda1e9fb22910ed7ac90fb1dac', 2),
+(53, 'Prince', 'Malkaniya', 'prince@gmail.com', 'cf509153db3d84c017c37f0e50e5c1cd', 3);
+
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `department`
---
-ALTER TABLE `department`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `advisor_details`
@@ -272,24 +266,22 @@ ALTER TABLE `advisor_details`
   ADD KEY `advisor_details_ibfk_3` (`title_id`);
 
 --
--- Indexes for table `terms`
+-- Indexes for table `department`
 --
-ALTER TABLE `terms`
+ALTER TABLE `department`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `users`
+-- Indexes for table `internship_date`
 --
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `rol_id` (`rol_id`);
+ALTER TABLE `internship_date`
+  ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `student_details`
+-- Indexes for table `internship_registration`
 --
-ALTER TABLE `student_details`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `student_details_ibfk_1` (`ogrenci_id`);
+ALTER TABLE `internship_registration`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `roles`
@@ -304,15 +296,16 @@ ALTER TABLE `social_security`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `internship_registration`
+-- Indexes for table `student_details`
 --
-ALTER TABLE `internship_registration`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `student_details`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `student_details_ibfk_1` (`ogrenci_id`);
 
 --
--- Indexes for table `Internship_date`
+-- Indexes for table `terms`
 --
-ALTER TABLE `Internship_date`
+ALTER TABLE `terms`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -322,14 +315,15 @@ ALTER TABLE `titles`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- Indexes for table `users`
 --
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `rol_id` (`rol_id`);
 
 --
--- AUTO_INCREMENT for table `department`
+-- AUTO_INCREMENT for dumped tables
 --
-ALTER TABLE `department`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `advisor_details`
@@ -338,22 +332,22 @@ ALTER TABLE `advisor_details`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
--- AUTO_INCREMENT for table `terms`
+-- AUTO_INCREMENT for table `department`
 --
-ALTER TABLE `terms`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+ALTER TABLE `department`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT for table `internship_date`
 --
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
+ALTER TABLE `internship_date`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
--- AUTO_INCREMENT for table `student_details`
+-- AUTO_INCREMENT for table `internship_registration`
 --
-ALTER TABLE `student_details`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+ALTER TABLE `internship_registration`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `roles`
@@ -368,22 +362,28 @@ ALTER TABLE `social_security`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT for table `internship_registration`
+-- AUTO_INCREMENT for table `student_details`
 --
-ALTER TABLE `internship_registration`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+ALTER TABLE `student_details`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
--- AUTO_INCREMENT for table `Internship_date`
+-- AUTO_INCREMENT for table `terms`
 --
-ALTER TABLE `Internship_date`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+ALTER TABLE `terms`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `titles`
 --
 ALTER TABLE `titles`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
 
 --
 -- Constraints for dumped tables
@@ -398,16 +398,16 @@ ALTER TABLE `advisor_details`
   ADD CONSTRAINT `advisor_details_ibfk_3` FOREIGN KEY (`title_id`) REFERENCES `titles` (`id`) ON DELETE CASCADE;
 
 --
--- Constraints for table `users`
---
-ALTER TABLE `users`
-  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`rol_id`) REFERENCES `roles` (`id`);
-
---
 -- Constraints for table `student_details`
 --
 ALTER TABLE `student_details`
   ADD CONSTRAINT `student_details_ibfk_1` FOREIGN KEY (`ogrenci_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`rol_id`) REFERENCES `roles` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
